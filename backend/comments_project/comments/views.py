cf4ddc04a936117066af9d88d831029aee9467c8
root <root@DESKTOP-QNA7NCI.localdomain>
2025-09-10 15:12:48 +0300

    Completed step 3: Added PreviewView, fixed signals, added JWT endpoints, tested API

--- backend/comments_project/comments/views.py
+++ backend/comments_project/comments/views.py
@@ -4,6 +4,8 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer
+from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 
 class CommentListCreateView(generics.ListCreateAPIView):
     queryset = Comment.objects.filter(parent__isnull=True)  # Тільки кореневі коментарі
@@ -13,4 +15,13 @@ class CommentListCreateView(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['user_name', 'email', 'created_at']
     ordering_fields = ['user_name', 'email', 'created_at']
-    pagination_class = 'rest_framework.pagination.PageNumberPagination'
+    pagination_class = PageNumberPagination
+
+class PreviewView(APIView):
+    def post(self, request):
+        text = request.data.get('text', '')
+        ALLOWED_TAGS = ['a', 'code', 'i', 'strong']
+        ALLOWED_ATTRIBUTES = {'a': ['href', 'title']}
+        cleaned_text = bleach.clean(text, tags=ALLOWED_TAGS, attributes=ALLOWED_ATTRIBUTES)
+        return Response({'preview': cleaned_text})
+
