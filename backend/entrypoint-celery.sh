cd1bac0f3ae43b2dd61351a03763f13d3f8d7d34
root <root@DESKTOP-S3933CE.localdomain>
2025-09-27 12:19:34 +0300

    prepare redis to deploy3

--- backend/entrypoint-celery.sh
+++ backend/entrypoint-celery.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e  # вихід при будь-якій помилці
+set -e
 
 export PYTHONPATH=/app/comments_project:$PYTHONPATH
 
-# --- PostgreSQL ---
+# PostgreSQL
 echo "Waiting for PostgreSQL..."
-export PGPASSWORD="${POSTGRES_PASSWORD:-user_password}"  # бери з env або дефолт
+export PGPASSWORD="${POSTGRES_PASSWORD:-user_password}"
 counter=0
 max_attempts=30
 until pg_isready -h "${POSTGRES_HOST:-postgres}" -U "${POSTGRES_USER:-user}" -d "${POSTGRES_DB:-comments_db}"; do
@@ -13,35 +13,30 @@ until pg_isready -h "${POSTGRES_HOST:-postgres}" -U "${POSTGRES_USER:-user}" -d 
     sleep 2
     counter=$((counter+1))
     if [ $counter -ge $max_attempts ]; then
-        echo "PostgreSQL is still unavailable after $max_attempts attempts, exiting"
+        echo "PostgreSQL still unavailable after $max_attempts attempts"
         exit 1
     fi
 done
-echo "PostgreSQL is ready!"
+echo "PostgreSQL ready!"
 
-# --- Redis ---
+# Redis
 if [ -z "$REDIS_URL" ]; then
     echo "REDIS_URL is not set. Exiting."
     exit 1
 fi
 
-# Витягуємо host, port і password з REDIS_URL
-REDIS_HOST=$(echo "$REDIS_URL" | sed -E 's|redis://([^:]+):([^@]+)@([^:]+):([0-9]+)/.*|\3|')
-REDIS_PORT=$(echo "$REDIS_URL" | sed -E 's|redis://([^:]+):([^@]+)@([^:]+):([0-9]+)/.*|\4|')
-REDIS_PASSWORD=$(echo "$REDIS_URL" | sed -E 's|redis://([^:]+):([^@]+)@([^:]+):([0-9]+)/.*|\2|')
-
-echo "Waiting for Redis at $REDIS_HOST:$REDIS_PORT..."
+echo "Waiting for Redis at $REDIS_URL..."
 counter=0
-until redis-cli -h "$REDIS_HOST" -p "$REDIS_PORT" -a "$REDIS_PASSWORD" ping | grep -q PONG; do
+until redis-cli -u "$REDIS_URL" ping | grep -q PONG; do
     echo "Redis is unavailable - sleeping"
     sleep 2
     counter=$((counter+1))
     if [ $counter -ge $max_attempts ]; then
-        echo "Redis is still unavailable after $max_attempts attempts, exiting"
+        echo "Redis still unavailable after $max_attempts attempts"
         exit 1
     fi
 done
-echo "Redis is ready!"
+echo "Redis ready!"
 
-# --- Запуск CMD (Django, Celery, etc) ---
+# Запуск CMD
 exec "$@"
