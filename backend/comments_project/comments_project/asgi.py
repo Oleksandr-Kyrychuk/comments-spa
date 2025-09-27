87d3b949cbb341cb1175c97f16df524307d14fb5
root <root@DESKTOP-S3933CE.localdomain>
2025-09-27 17:03:54 +0300

    fix deploy3

--- backend/comments_project/comments_project/asgi.py
+++ backend/comments_project/comments_project/asgi.py
@@ -3,19 +3,15 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 
+from comments import routing  # Імпорт тут, не в функції
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'comments_project.settings')
 
-# Ініціалізуємо Django ASGI додаток
 django_asgi_app = get_asgi_application()
 
-# Відкладаємо імпорт routing до моменту, коли він буде потрібен
-def get_application():
-    from comments import routing
-    return ProtocolTypeRouter({
-        "http": django_asgi_app,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(routing.websocket_urlpatterns)
-        ),
-    })
-
-application = get_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(routing.websocket_urlpatterns)
+    ),
+})
